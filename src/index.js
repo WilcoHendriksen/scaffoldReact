c602a51326c9ad8dd88deca2fb816896a4c640fe
Wilco Hendriksen <hendriksenwj@gmail.com>
2020-02-02 22:32:01 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div style={{width: '100vw', height: '100vh'}}>
+        scafoldded react app
+    </div>
+}
+
+ReactDOM.render(
+    <App /> ,
+    document.getElementById("root")
+)
