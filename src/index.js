00286cacce0152d181d41326603f88a4580c3607
Wilco Hendriksen <whendriksen@npqmail.com>
2020-02-03 22:05:58 +0100

    more scaffolding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from './components/Button'
 
 const App = () => {
-    return <div style={{width: '100vw', height: '100vh'}}>
-        scafoldded react app
+    return <div style={{width: '100vw', height: '100vh', color: 'red'}}>
+        scafoldded react app, bbbhh
+        <Button onClick={() => alert('hi')}>test</Button>
     </div>
 }
 
